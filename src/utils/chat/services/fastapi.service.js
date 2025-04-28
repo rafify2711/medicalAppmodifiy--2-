f3164702306e52e0a6f7a485f3598fce7eba16ed
ranim afify <ranimafify27@gmail.com>
2025-04-28 22:01:11 +0300

    edit path to chatbot

--- src/utils/chat/services/fastapi.service.js
+++ src/utils/chat/services/fastapi.service.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const askChatbot = async (prompt) => {
     try {
-        const response = await axios.post(`https://medical-ai-production.up.railway.app//chat/?prompt=${encodeURIComponent(prompt)}`);
+        const response = await axios.post(`https://medical-ai-production.up.railway.app/chat/?prompt=${encodeURIComponent(prompt)}`);
         return response.data;
       } catch (error) {
         console.error('Error contacting chatbot:', error.message);
